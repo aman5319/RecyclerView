d9df46e84a830391b3ed6c7588aa5cc22afc1172
Amidezcod <amanpandey5319@gmail.com>
2017-06-24 23:40:09 +0530

    update AppListFragment with RecyclerView ,CardView having imageview and textview with drag and drop (minimal) use gradle dependency from AdvancedRecyclerView

--- app/src/main/java/com/example/amidezcod/advancedrecyclerview/AppsListFragment.java
+++ app/src/main/java/com/example/amidezcod/advancedrecyclerview/AppsListFragment.java
@@ -1,15 +1,24 @@
 package com.example.amidezcod.advancedrecyclerview;
 
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.h6ah4i.android.widget.advrecyclerview.draggable.DraggableItemAdapter;
+import com.h6ah4i.android.widget.advrecyclerview.draggable.ItemDraggableRange;
+import com.h6ah4i.android.widget.advrecyclerview.draggable.RecyclerViewDragDropManager;
+import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractDraggableItemViewHolder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +28,21 @@ import java.util.List;
  */
 
 public class AppsListFragment extends Fragment {
+    private List<ApplicationInfo> packages;
+    private PackageManager packageManager;
+
+    @Override
+    public void setArguments(Bundle args) {
+        super.setArguments(args);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        packageManager = context.getPackageManager();
+        packages = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
+    }
+
     /**
      * Called to have the fragment instantiate its user interface view.
      * This is optional, and non-graphical fragments can return null (which
@@ -41,25 +65,115 @@ public class AppsListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.list_apps_fragment, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.list_item_app);
 
-        ListView listView = view.findViewById(R.id.list_item);
-        ArrayList<String> names = new ArrayList<>();
-        PackageManager packageManager = getActivity().getPackageManager();
-        List<ApplicationInfo> packages = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
-        for (ApplicationInfo infoc : packages) {
-            //use Delimter for . operator
-            String[] strings = infoc.packageName.split("\\.");
-            names.add(capitalizeFirstLetter(strings[strings.length - 1]));
-        }
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, names);
-        listView.setAdapter(arrayAdapter);
+        // Setup D&D feature and RecyclerView
+        RecyclerViewDragDropManager dragMgr = new RecyclerViewDragDropManager();
+
+        dragMgr.setInitiateOnMove(false);
+        dragMgr.setInitiateOnLongPress(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(dragMgr.createWrappedAdapter(new MyAdapter()));
+
+        dragMgr.attachRecyclerView(recyclerView);
+
         return view;
     }
 
-    public String capitalizeFirstLetter(String original) {
-        if (original == null || original.length() == 0) {
-            return original;
+    private static class MyItem {
+        public final long id;
+        public final String text;
+        public Drawable imageDrawable;
+
+        public MyItem(long id, String text, Drawable imageId) {
+            this.id = id;
+            this.text = text;
+            this.imageDrawable = imageId;
         }
-        return original.substring(0, 1).toUpperCase() + original.substring(1);
+    }
+
+    static class MyViewHolder extends AbstractDraggableItemViewHolder {
+        TextView textView;
+        ImageView imageView;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            textView =  itemView.findViewById(R.id.text_app_list);
+            imageView = itemView.findViewById(R.id.image_app_list);
+        }
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> implements DraggableItemAdapter<MyViewHolder> {
+        List<MyItem> mItems;
+
+
+        public MyAdapter() {
+            setHasStableIds(true); // this is required for D&D feature.
+            mItems = new ArrayList<>();
+            for (int i = 0; i < packages.size(); i++) {
+                String[] strings = packages.get(i).packageName.split("\\.");
+                String s = capitalizeFirstLetter(strings[strings.length - 1]);
+                mItems.add(new MyItem(i, s, packages.get(i).loadIcon(packageManager)));
+            }
+
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return mItems.get(position).id; // need to return stable (= not change even after reordered) value
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_for_app_list, parent, false);
+            return new MyViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            MyItem item = mItems.get(position);
+            holder.textView.setText(item.text);
+            holder.imageView.setImageDrawable(item.imageDrawable);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItems.size();
+        }
+
+        @Override
+        public void onMoveItem(int fromPosition, int toPosition) {
+            MyItem movedItem = mItems.remove(fromPosition);
+            mItems.add(toPosition, movedItem);
+            notifyItemMoved(fromPosition, toPosition);
+        }
+
+        @Override
+        public boolean onCheckCanStartDrag(MyViewHolder holder, int position, int x, int y) {
+            return true;
+        }
+
+        @Override
+        public ItemDraggableRange onGetItemDraggableRange(MyViewHolder holder, int position) {
+            return null;
+        }
+
+        @Override
+        public boolean onCheckCanDrop(int draggingPosition, int dropPosition) {
+            return true;
+        }
+
+        private String capitalizeFirstLetter(String original) {
+            if (original == null || original.length() == 0) {
+                return original;
+            }
+            return original.substring(0, 1).toUpperCase() + original.substring(1);
+
+
+        }
     }
 }
+
+
+
