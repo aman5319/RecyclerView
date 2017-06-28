251b3c94d25d4b8b85c008bf6576a39b1940a8f2
Amidezcod <amanpandey5319@gmail.com>
2017-06-29 00:45:36 +0530

    change in layout of card_for_app_list.xml update in Layoutmanager for app list fragment from LinearLayoutManager to GridLayoutManager

--- app/src/main/java/com/example/amidezcod/advancedrecyclerview/AppsListFragment.java
+++ app/src/main/java/com/example/amidezcod/advancedrecyclerview/AppsListFragment.java
@@ -9,7 +9,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -75,7 +75,7 @@ public class AppsListFragment extends Fragment {
         dragMgr.setInitiateOnMove(false);
         dragMgr.setInitiateOnLongPress(true);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
         recyclerView.setAdapter(dragMgr.createWrappedAdapter(new MyAdapter()));
 
         dragMgr.attachRecyclerView(recyclerView);
